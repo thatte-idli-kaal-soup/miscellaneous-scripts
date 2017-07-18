961cb90ad210d0e12e5e731f03b07acf6895a1be
Puneeth Chaganti <punchagan@muse-amuse.in>
2017-07-18 23:19:10 +0530

    Add passes histogram for events

--- stats.py
+++ stats.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from collections import OrderedDict
+from collections import Counter, OrderedDict
 import os
 from os.path import abspath, dirname, exists, join
 
@@ -178,6 +178,141 @@ def time_between_points():
     return table
 
 
+def passes_histogram(chosen_opponent=None):
+    opponent_data = {}
+    for opponent in OPPONENTS:
+        events = get_match_events(TOURNAMENT, opponent)
+        goals = events[(events['Event Type'] == 'Offense') &
+                       (events['Action'] == 'Goal')]
+        passes = events[(events['Event Type'] == 'Offense') &
+                         (events['Action'] == 'Catch')]
+        drops = events[(events['Event Type'] == 'Offense') &
+                       (events['Action'] == 'Drop')]
+        throwaways = events[
+            (events['Event Type'] == 'Offense') &
+            (events['Action'] == 'Throwaway')
+        ]
+
+        # Possession gaining events -
+        # defense-goal, defense-throwaway, defense-d
+        possession_change_events = events[
+            (events['Event Type'] == 'Defense') &
+            (
+                (events['Action'] == 'Goal') |
+                (events['Action'] == 'Throwaway') |
+                (events['Action'] == 'D')
+            )
+        ]
+
+        def passes_since_possession(event_index):
+            indexes = possession_change_events.index[
+                possession_change_events.index < event_index
+            ]
+            possession = (
+                indexes[-1] if len(indexes) > 0 else (events.index[0] - 1)
+            )
+            return event_index - possession
+
+        opponent_data[opponent] = {
+            'goals': goals.index.map(passes_since_possession),
+            'passes': passes.index.map(passes_since_possession),
+            'drops': drops.index.map(passes_since_possession),
+            'throwaways': throwaways.index.map(passes_since_possession),
+        }
+
+    if chosen_opponent:
+        data = opponent_data[chosen_opponent]
+        goals = data['goals']
+        passes = data['passes']
+        drops = data['drops']
+        throwaways = data['throwaways']
+
+    else:
+        data = opponent_data.values()
+        goals = [
+            count
+            for value in data
+            for count in value['goals']
+        ]
+        passes = [
+            count
+            for value in data
+            for count in value['passes']
+        ]
+        drops = [
+            count
+            for value in data
+            for count in value['drops']
+        ]
+        throwaways = [
+            count
+            for value in data
+            for count in value['throwaways']
+        ]
+
+    goals = Counter(goals)
+    passes = Counter(passes)
+    drops = Counter(drops)
+    throwaways = Counter(throwaways)
+
+    turnovers = Counter()
+    turnovers.update(drops)
+    turnovers.update(throwaways)
+
+    catches = Counter()
+    catches.update(passes)
+    catches.update(goals)
+
+    data = [
+        go.Bar(
+            x=list(goals.keys()),
+            y=list(goals.values()),
+            name='Goals',
+        ),
+        go.Bar(
+            x=list(passes.keys()),
+            y=list(passes.values()),
+            name='Passes',
+        ),
+        go.Bar(
+            x=list(catches.keys()),
+            y=list(catches.values()),
+            name='Catches',
+        ),
+        go.Bar(
+            x=list(drops.keys()),
+            y=list(drops.values()),
+            name='Drops',
+        ),
+        go.Bar(
+            x=list(throwaways.keys()),
+            y=list(throwaways.values()),
+            name='Throwaways',
+        ),
+        go.Bar(
+            x=list(turnovers.keys()),
+            y=list(turnovers.values()),
+            name='Turnovers',
+        ),
+    ]
+
+    figure = {
+        'data': data,
+        'layout': {
+            'xaxis': {
+                'title': 'Pass # for event',
+                'dtick': 1,
+            },
+            'yaxis': {
+                'title': '# of occurrences',
+            }
+
+        },
+    }
+
+    return figure
+
+
 def time_between_points_div():
     return html.P(
         "We look at median time (and not mean time) to compensate for not"
@@ -189,11 +324,12 @@ def time_between_points_div():
 graph_types = OrderedDict(
     [('Score Line', score_line_figure),
      ('O-D Lines', o_d_lines_figure),
-     ('Time between points', time_between_points), ]
+     ('Time between points', time_between_points),
+     ('Passes histogram', passes_histogram), ]
 )
 
 div_types = OrderedDict(
-    [('Time between points', time_between_points_div)]
+    [('Time between points', time_between_points_div), ]
 )
 
 
@@ -216,6 +352,12 @@ app.layout = html.Div([
                  for key in list(graph_types.keys())],
         value='Score Line',
     ),
+    dcc.Dropdown(
+        id='opponent-dropdown',
+        options=[{'label': opponent, 'value': opponent}
+                 for opponent in OPPONENTS] + [{'label': 'All', 'value': None}],
+        value=None
+    ),
     dcc.Graph(id='my-graph'),
     html.Div(id='my-div')
 ], className="container")
@@ -226,9 +368,20 @@ app.css.append_css({
 
 
 @app.callback(Output('my-graph', 'figure'),
-              [Input('graph-type-dropdown', 'value'), ])
-def update_graph(graph_type):
-    return graph_types[graph_type]() if graph_type in graph_types else ''
+              [Input('graph-type-dropdown', 'value'),
+               Input('opponent-dropdown', 'value')])
+def update_graph(graph_type, opponent):
+    if graph_type not in graph_types:
+        return None
+    f = graph_types[graph_type]
+    return f() if f.__code__.co_argcount == 0 else f(opponent)
+
+
+@app.callback(Output('opponent-dropdown', 'disabled'),
+              [Input('graph-type-dropdown', 'value')])
+def show_or_hide_opponent_dropdown(graph_type):
+    f = graph_types[graph_type]
+    return True if f.__code__.co_argcount == 0 else False
 
 
 @app.callback(Output('my-div', 'children'),
