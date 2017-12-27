507dee9ea38bd91ef34327f0eb242a462d1af509
Puneeth Chaganti <punchagan@muse-amuse.in>
2017-12-27 21:55:16 +0530

    Add js console snippet to create a new group

---
+++ splitwise.js
@@ -0,0 +1,18 @@
+// Browser snippet to create a new Splitwise group
+
+// 1. Go to the edit page of a group
+// 2. Run this javascript in the console
+
+var names = [
+    // ['Name', 'Email'],
+];
+
+names.map(function([name, email]){
+    document.querySelector('.add_nested_fields').click();
+});
+
+names.map(function([name, email], i){
+    console.log(i);
+    document.querySelectorAll('.name')[i].value = name;
+    document.querySelectorAll('.email')[i].value = email;
+});
