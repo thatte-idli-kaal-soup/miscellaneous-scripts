56f53b4f705161601bdd9670fcf1c7dd8fea7c12
Puneeth Chaganti <punchagan@muse-amuse.in>
2021-07-14 08:31:50 +0530

    whatsapp-diff: Format with prettier

--- whatsapp-diff.js
+++ whatsapp-diff.js
@@ -5,23 +5,23 @@
 // 3. Navigate to the second group and click the bookmark. Open the browser
 // console to see the diff output printed.
 
-javascript: void (function() {
-    var diff = function(a, b){
+javascript: void (function () {
+    var diff = function (a, b) {
         console.log(`List of people in ${a.name} but not in ${b.name}`);
         var missing = [];
-        a.members.map(function(name){
-            if (!b.members.includes(name)){
+        a.members.map(function (name) {
+            if (!b.members.includes(name)) {
                 missing.push(name);
             }
         });
         console.log(missing.join('\n'));
     };
-    var elements = document.querySelectorAll("#main span[title]"),
+    var elements = document.querySelectorAll('#main span[title]'),
         group_name = elements[0].textContent,
         names = elements[1].textContent.replace(/,/g, '').split(' '),
-        group = {name: group_name, members: names};
+        group = { name: group_name, members: names };
 
-    if (! window.diffGroups) {
+    if (!window.diffGroups) {
         window.diffGroups = [];
         window.diffGroups.push(group);
     } else {
@@ -30,4 +30,4 @@ javascript: void (function() {
         diff(group, group_b);
         diff(group_b, group);
     }
-}());
+})();
